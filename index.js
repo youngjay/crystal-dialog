aa33a94dafd4bbc3e1c32d81825910788f2b4844
杨杰 <hiyoungjay@gmail.com>
2014-04-29 23:25:50 +0800

    d

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-module.exports = function(componentFactory, options, buildDialogElement) {
-    var cache = {};
-    var activeDialogs = [];
-    if (!buildDialogElement) {
-        buildDialogElement = require('./lib/build-bootstrap3-dialog-element');
+module.exports = require('mixin-class')(
+    function(dialogOptions, dialogBuilder) {
+        this.Base = require('mixin-class')(require('./lib/model')((require('./lib/build-bootstrap3-dialog-element') || dialogBuilder)(dialogOptions)));
+        this.cache = {};
+        this.actives = [];
+    },
+    {
+        _removeFromActiveDialogs: function(dialog) {
+            var index = this.actives.indexOf(dialog);
+            if (index !== -1) {
+                this.actives.splice(index, 1);
+            }
+        },
+
+        add: function(type, component) {
+            this.cache[type] = component;
+        },
+
+        open: function(type, data, callback, context) {
+            var dialog = this.cache[type];
+            if (!dialog) {
+                throw new Error('dialog:' + type + ' not exists')
+            }
+            if (!dialog.__dialog) {
+                dialog = this.Base.extend(dialog).create()
+                dialog.render();
+                this.cache[type] = dialog;
+            }
+            this.actives.push(dialog);
+            var self = this;
+            dialog.open(data, function() {
+                self._removeFromActiveDialogs(dialog);
+                callback.apply(context, arguments);
+            })
+        }
     }
-    var Base = require('mixin-class')(require('./lib/model')(buildDialogElement(options || {})));
-
-    var removeFromActiveDialogs = function(dialog) {
-        var index = activeDialogs.indexOf(dialog);
-        if (index !== -1) {
-            activeDialogs.splice(index, 1);
-        }
-    };
-
-    var openDialog = function(type, data, callback, context) {
-        var dialog = cache[type];
-        if (!dialog) {
-            dialog = Base.extend(componentFactory(type)).create();
-            dialog.render();
-            cache[type] = dialog;
-        }
-        activeDialogs.push(dialog);
-        dialog.open(data, function() {
-            removeFromActiveDialogs(dialog);
-            callback.apply(context, arguments);
-        });
-    };
-
-    openDialog.activeDialogs = activeDialogs;
-
-    return openDialog;
-};
-
+)
