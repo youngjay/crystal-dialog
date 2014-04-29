db90ccb8e0a20432ef435e2d314e25d7a5a8572d
杨杰 <hiyoungjay@gmail.com>
2014-04-30 00:02:02 +0800

    refactor dialog

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
 module.exports = require('mixin-class')(
     function(dialogOptions, dialogBuilder) {
         this.Base = require('mixin-class')(require('./lib/model')((require('./lib/build-bootstrap3-dialog-element') || dialogBuilder)(dialogOptions)));
-        this.cache = {};
         this.actives = [];
     },
     {
-        _removeFromActiveDialogs: function(dialog) {
+        addToActives: function(dialog) {
+            this.actives.push(dialog);
+        },
+
+        getActives: function() {
+            return this.actives.slice();
+        },
+
+        removeFromActives: function(dialog) {
             var index = this.actives.indexOf(dialog);
             if (index !== -1) {
                 this.actives.splice(index, 1);
             }
         },
 
-        add: function(type, component) {
-            this.cache[type] = component;
-        },
-
-        open: function(type, data, callback, context) {
-            var dialog = this.cache[type];
-            if (!dialog) {
-                throw new Error('dialog:' + type + ' not exists')
-            }
-            if (!dialog.__dialog) {
-                dialog = this.Base.extend(dialog).create()
-                dialog.render();
-                this.cache[type] = dialog;
-            }
-            this.actives.push(dialog);
-            var self = this;
-            dialog.open(data, function() {
-                self._removeFromActiveDialogs(dialog);
-                callback.apply(context, arguments);
-            })
+        create: function(component) {
+            return this.Base.extend(component).create(this);
         }
     }
 )
