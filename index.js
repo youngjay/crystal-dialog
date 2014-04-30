1a5adaa47111d21191edb2b46df4b64609ad2665
yangjie <hiyoungjay@gmail.com>
2014-04-30 09:57:39 +0800

    change interface

--- index.js
+++ index.js
@@ -1,26 +1,69 @@
+var mixin = require('mixin-class');
+var noop = function() {};
+var ko = require('knockout');
+
+var parseTemplate = function(html) {
+    var fragments = ko.utils.parseHtmlFragment(html);
+    for (var i = 0; i < fragments.length; i++) {
+        if (fragments[i].nodeType === 1) {
+            return fragments[i];
+        }
+    }
+};
+
+
 module.exports = require('mixin-class')(
     function(dialogOptions, dialogBuilder) {
-        this.Base = require('mixin-class')(require('./lib/model')((require('./lib/build-bootstrap3-dialog-element') || dialogBuilder)(dialogOptions)));
-        this.actives = [];
+        var actives = this.actives = [];
+
+        var buildDialogElement = (require('./lib/build-bootstrap3-dialog-element') || dialogBuilder)(dialogOptions);
+
+        this._mixins = {
+            _open: function(callback) {
+                if (!this.__dialog) {
+                    this._render();
+                }
+                actives.push(this);
+                this.__dialogCallback = callback || noop;
+                this.__dialog.show();
+            },
+
+            close: function() {
+                var index = actives.indexOf(this);
+                if (index !== -1) {
+                    actives.splice(index, 1);
+                }
+                this.__dialog.hide();
+                this.__dialogCallback.apply(this, arguments);
+            },
+
+            _render: function() {
+                var element = parseTemplate(this.__view);
+                document.body.appendChild(element);
+
+                ko.applyBindings(this, element);
+                this.__dialog = buildDialogElement(element);
+            }
+        };
+
+        this.cache = {};
     },
     {
-        addToActives: function(dialog) {
-            this.actives.push(dialog);
-        },
-
         getActives: function() {
             return this.actives.slice();
         },
 
-        removeFromActives: function(dialog) {
-            var index = this.actives.indexOf(dialog);
-            if (index !== -1) {
-                this.actives.splice(index, 1);
-            }
+        add: function(type, component, handleOpen) {
+            this.cache[type] = {
+                handleOpen: handleOpen,
+                dialog: component.mix(this._mixins)
+            };
         },
 
-        create: function(component) {
-            return this.Base.extend(component).create(this);
+        open: function(type, data, callback) {
+            var o = this.cache[type];
+            o.handleOpen(data);
+            o.dialog._open(callback);
         }
     }
 )
